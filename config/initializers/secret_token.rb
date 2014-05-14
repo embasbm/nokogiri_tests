9aabbfe2ee01c9b752ee5d22ed0145e28c62faf7
emba <emba@linedmedia.com.au>
2014-05-14 11:02:48 +1000

    structure rails project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NokogiriTests::Application.config.secret_key_base = 'da2cfb1defe384fe54e2c7f35f2c83b3040b40f41981bb3d058b654e15c8b71261c214955c060b4a23915864872f185e8b5f495190baeb4e7250d5825f4471c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NokogiriTests::Application.config.session_store :cookie_store, key: '_nokogiri_tests_session'
